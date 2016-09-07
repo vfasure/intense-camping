f6ec789a271a0307bd9878bb29ed1bf20555a9db
simpsoka <simpsoka@gmail.com>
2016-09-07 13:08:23 -0700

    update process

--- app.js
+++ app.js
@@ -10,6 +10,6 @@ app.get('/', function(req, res){
   res.render('index', { season: 'summer'});
 });
 
-app.listen(3000, function () {
+app.listen(process.env.PORT || 3000, function() {
   console.log('Example app listening on port 3000!');
 });
