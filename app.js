c3861f7937aa55b0301b7714ea35fb8dd18d95e2
kathysimpson <simpsoka@gmail.com>
2016-09-13 19:00:42 -0700

    testing without pg stubbing

--- app.js
+++ app.js
@@ -1,7 +1,8 @@
-if (!process.env.DATABASE_URL) {
-  console.error('😵  DATABASE_URL is required 😵');
-  process.exit(0);
-}
+
+// if (!process.env.DATABASE_URL) {
+//  console.error('😵  DATABASE_URL is required 😵');
+//  process.exit(0);
+// }
 
 var express = require('express')
 var app = express();
@@ -9,21 +10,21 @@ var bodyParser = require('body-parser')
 var trips = require('./data/trips');
 var _ = require('underscore');
 
-var Knex = require('knex');
-var URL = require('url');
-var pg_server = URL.parse(process.env.DATABASE_URL, true);
-var knex = Knex({
-  client: 'pg',
-  pool: { min: 0, max: 7 },
-  connection: {
-    host: pg_server.hostname,
-    port: pg_server.port,
-    ssl: pg_server.query.ssl,
-    database: pg_server.pathname.substring(1),
-    user: pg_server.auth.split(':')[0],
-    password: pg_server.auth.split(':')[1]
-  }
-});
+// var Knex = require('knex');
+// var URL = require('url');
+// var pg_server = URL.parse(process.env.DATABASE_URL, true);
+// var knex = Knex({
+//   client: 'pg',
+//   pool: { min: 0, max: 7 },
+//   connection: {
+//     host: pg_server.hostname,
+//     port: pg_server.port,
+//     ssl: pg_server.query.ssl,
+//     database: pg_server.pathname.substring(1),
+//     user: pg_server.auth.split(':')[0],
+//     password: pg_server.auth.split(':')[1]
+//   }
+// });
 
 app.use(express.static('public'));
 app.use(bodyParser.json()); // to support JSON-encoded bodies
@@ -45,13 +46,13 @@ app.get('/reservations/new', function(req, res) {
 });
 
 app.post('/reservations/new', function(req, res){
-  knex('tableName').insert({
-    req.body.name,
-    req.body.email,
-    req.body.trip_id
-  }).then(function() {
-    res.sendStatus(201);
-  })
+  // knex('tableName').insert({
+  //   req.body.name,
+  //   req.body.email,
+  //   req.body.trip_id
+  // }).then(function() {
+  //   res.sendStatus(201);
+  // })
 });
 
 app.listen(process.env.PORT || 3000, function() {
