fe6e49294deae60bf5eda6ea5068c6109cdc9c82
Johnny Domino <jvdomino@gmail.com>
2016-09-15 13:44:13 -0700

    read `trips` from DB; write `reservations` to DB

--- migrations/20160915130725_reservations.js
+++ migrations/20160915130725_reservations.js
@@ -1,6 +1,7 @@
 exports.up = function(knex, Promise) {
   return knex.schema.createTable('reservations', function(t) {
     t.increments('id');
+    t.integer('trip_id').references('id').inTable('trips');
     t.string('name', 2048);
     t.string('email', 2048);
   });
