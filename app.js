971660f1bb95c4ec9540f51eb1478b6aa171c888
simpsoka <simpsoka@gmail.com>
2016-10-04 09:39:46 -0700

    language change

--- app.js
+++ app.js
@@ -15,7 +15,7 @@ app.set('views', __dirname + '/views');
 app.set('view engine', 'jade');
 
 app.get('/', function(req, res){
-  res.render('index', { season: 'summer'});
+  res.render('index', { intensity: 'chill'});
 });
 
 app.get('/trips', function(req, res) {
