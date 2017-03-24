785c4fcbf4dda7048460e0e971b4c1ba5a469e00
simpsoka <simpsoka@gmail.com>
2017-03-24 07:52:06 -0700

    reset demo

--- pages/index.js
+++ pages/index.js
@@ -3,7 +3,7 @@ import Head from 'next/head'
 import Link from 'next/link'
 
 export default () => {
-  const intensity = 'instense'
+  const intensity = 'camping'
 
   return (
     <div className="root">
