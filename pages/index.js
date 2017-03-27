a5d3e1efec46f16f24adf75de11aee0ffdfd6318
simpsoka <simpsoka@gmail.com>
2017-03-27 13:20:22 -0700

    ci demo

--- pages/index.js
+++ pages/index.js
@@ -3,7 +3,7 @@ import Head from 'next/head'
 import Link from 'next/link'
 
 export default () => {
-  const intensity = 'camping'
+  const intensity = 'happy monday LIVE demo'
 
   return (
     <div className="root">
