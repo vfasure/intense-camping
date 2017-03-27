8ce76560ca343c2e50a29f8695f12a88dda068ad
simpsoka <simpsoka@gmail.com>
2017-03-27 08:46:58 -0700

    Update index.js

--- pages/index.js
+++ pages/index.js
@@ -3,7 +3,7 @@ import Head from 'next/head'
 import Link from 'next/link'
 
 export default () => {
-  const intensity = 'Happy Monday Live Demo'
+  const intensity = 'camping'
 
   return (
     <div className="root">
