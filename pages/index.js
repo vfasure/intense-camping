45db367b7002ee7f5a0692b724e5e3ceebddcc7c
simpsoka <simpsoka@gmail.com>
2017-03-26 19:59:13 -0700

    awesome platform demo

--- pages/index.js
+++ pages/index.js
@@ -3,7 +3,7 @@ import Head from 'next/head'
 import Link from 'next/link'
 
 export default () => {
-  const intensity = 'camping'
+  const intensity = 'intense-camping'
 
   return (
     <div className="root">
