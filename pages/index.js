3dfe3d282dc22f384e7914f209105f9af062f3d3
simpsoka <simpsoka@gmail.com>
2017-03-26 20:04:48 -0700

    fixing tests

--- pages/index.js
+++ pages/index.js
@@ -3,7 +3,7 @@ import Head from 'next/head'
 import Link from 'next/link'
 
 export default () => {
-  const intensity = 'intense-camping'
+  const intensity = 'camping'
 
   return (
     <div className="root">
