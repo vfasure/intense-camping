09fbea08ad932e109f72f908e7431acf4bcd5a3d
simpsoka <simpsoka@gmail.com>
2017-06-20 13:24:23 -0700

    Update index.js

--- pages/index.js
+++ pages/index.js
@@ -3,7 +3,7 @@ import Head from 'next/head'
 import Link from 'next/link'
 
 export default () => {
-  const intensity = 'chill'
+  const intensity = 'intense'
 
   return (
     <div className="root">
