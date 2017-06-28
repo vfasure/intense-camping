910406722e92f2cc14a0e96e75acad94916f38c3
simpsoka <simpsoka@gmail.com>
2017-06-28 12:29:38 -0700

    intensity to 11

--- pages/index.js
+++ pages/index.js
@@ -3,7 +3,7 @@ import Head from 'next/head'
 import Link from 'next/link'
 
 export default () => {
-  const intensity = 'camping'
+  const intensity = 'intense'
 
   return (
     <div className="root">
